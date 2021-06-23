a57627cee63f623262563ad9b79a59d5292b6499
DESKTOP-3CSCETV\David <david_dag97_14@hotmail.com>
2021-06-23 20:02:34 +0200

    Iconos añadidos y main bien colocado

--- finalProyect/src/app/app-routing.module.ts
+++ finalProyect/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MenulateralComponent } from './menulateral/menulateral.component';
+//import { MenulateralComponent } from './menulateral/menulateral.component';
 import { ContentmainComponent } from './contentmain/contentmain.component';
 
 const routes: Routes = [
-  {path: 'sidebar', component: MenulateralComponent},
-  {path: '', component: ContentmainComponent}
+  {path: '', component: ContentmainComponent},
+ // {path: 'sidebar', component: MenulateralComponent}
 ];
 
 @NgModule({
