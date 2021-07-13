11ea0ec3bf7c62b48d945a157c69e68473b8d451
David Arreaza Gil <darreazagil@gmail.com>
2021-07-13 19:26:54 +0200

    Show all games

--- finalProyect/src/app/mispartidas/mispartidas.component.ts
+++ finalProyect/src/app/mispartidas/mispartidas.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NotifierService } from 'angular-notifier';
+import { Game } from '../shared/models/game';
+import { AuthService } from '../shared/services/auth.service';
+import { CrudGamesService } from '../shared/services/crud-games.service';
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-mispartidas',
@@ -7,9 +14,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MispartidasComponent implements OnInit {
 
-  constructor() { }
+  miGame : any;
+  idUser = '';
+  misGames: Array<Game> = [];
+  faTrashAlt = faTrashAlt;
+  faEdit = faEdit;
+
+  constructor(private authService : AuthService, private gameService : CrudGamesService,
+     private notifier : NotifierService, private router: Router) { }
 
   ngOnInit(): void {
+    this.idUser = this.authService.userData().uid;
+    this.readAllGamesUser();
+  }
+
+  readAllGamesUser(){
+    this.gameService.readAllGames().subscribe(data => {
+      this.misGames = [];
+      data.forEach((doc : any) => {
+        let newGame: Game = doc.data();
+        newGame.id = doc.id;
+        if(newGame.idUser.trim() == this.idUser.trim()){
+          this.misGames.push(newGame);
+        }
+      })
+      console.log(this.misGames)
+    })
+  }
+
+  deleteGame(idGame : any){
+    console.log("entro")
+    this.gameService.deleteGame(idGame).then(success => {
+      this.notifier.notify('success', "Partida eliminada");
+      this.readAllGamesUser();
+    }).catch(error => {
+      this.notifier.notify('error', "Error");
+    })
+  }
+
+  editPartida(idGame : any){
+    this.router.navigate(["editGame/" + idGame]);
   }
 
 }
