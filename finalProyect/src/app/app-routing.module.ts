bfac0481889dee2e3657e972f9a00abe70323dc6
David Arreaza Gil <darreazagil@gmail.com>
2021-07-15 17:04:22 +0200

    ~

--- finalProyect/src/app/app-routing.module.ts
+++ finalProyect/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MenulateralComponent } from './menulateral/menulateral.component';
 import { ContentmainComponent } from './contentmain/contentmain.component';
-import { LoginComponent } from './login/login.component';
 
 import { DetallesComponent } from './detalles/detalles.component';
 import { AuthGuard } from './shared/guard/auth.guard';
@@ -14,8 +12,7 @@ import { MispartidasComponent } from './mispartidas/mispartidas.component';
 const routes: Routes = [
   {path: '', component: ContentmainComponent, pathMatch: "full"},
   {path: 'home/:id', component: ContentmainComponent, canActivate: [AuthGuard]},
-  //{path: 'sidebar', component: MenulateralComponent}
-  //{path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+
   {path: 'edit/:id', component: EditperfilComponent, canActivate: [AuthGuard]},
   {path: 'detalles/:id', component: DetallesComponent, canActivate: [AuthGuard]},
   {path: 'add', component: AddGamesComponent, canActivate: [AuthGuard]},
